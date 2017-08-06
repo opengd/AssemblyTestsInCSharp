dc2ae24f9cd6ed7108ed4862e8219640e2d20f77
Erik Johansson <opengd@outlook.com>
2017-08-07 00:11:46 +0200

    Update a simple test of working asm code, viewing result of calc function

--- AssemblyTestsInCSharp/Program.cs
+++ AssemblyTestsInCSharp/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace AssemblyTestsInCSharp
 {
     class Program
     {
+        private delegate Int32 MyAdd(Int32 x, Int32 y, Int32 z);
+
+        [DllImport("kernel32.dll")]
+        static extern bool VirtualProtectEx(Int32 hProcess, System.IntPtr lpAddress, int dwSize, int flNewProtect, out int lpflOldProtect);
+
         static void Main(string[] args)
         {
+            var byteCodeArray = new Byte[]
+            {
+                0x8B, 0x54, 0x24, 0x0C,
+                0x8B, 0x44, 0x24, 0x04, // mov eax,dword ptr [esp+4]
+                0x8B, 0x4C, 0x24, 0x08, // mov ecx,dword ptr [esp+2]
+                0x40, 0x83, 0xF8, 0x0A, 0x7C, 0xFA,
+                0xC2, 0x0c, 0x00
+            };
+
+            GCHandle pinnedArray = GCHandle.Alloc(byteCodeArray, GCHandleType.Pinned);
+
+            IntPtr pointer = pinnedArray.AddrOfPinnedObject();
+
+            var hProcess = Process.GetCurrentProcess().Handle.ToInt32();
+
+            var myAdd = (MyAdd)Marshal.GetDelegateForFunctionPointer(pointer, typeof(MyAdd));
+
+            VirtualProtectEx(hProcess, pointer, byteCodeArray.Length, 0x40, out int flOldProtect);
+
+            Int32 result = 0;
+            try
+            {
+                result = myAdd(1, 2, 3);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                VirtualProtectEx(hProcess, pointer, byteCodeArray.Length, flOldProtect, out flOldProtect);
+                Console.WriteLine("Result: {0}", result);
+
+                pinnedArray.Free();
+            }
+
+            Console.ReadKey();
         }
     }
 }
